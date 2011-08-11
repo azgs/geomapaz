1a5f156dba88ee27e4fc81a63823f82c6373df0c
gchen <genhan.chen@azgs.az.gov>
2011-08-11 10:47:15 -0700

    Make logo available for different browsers

--- LOGO.js
+++ LOGO.js
@@ -11,9 +11,11 @@ function addLogo(){
 itemLogo = {
 		xtype: 'panel',
 		id: 'I_LOGO',
-		html: '<img id="LOGO" src="SRC/logo.png" width="64" height="64" style="opacity:0.75;"/>',
-		bodyStyle: 'position: absolute; z-index: 1100; background: none; right: 5px; bottom: 5px; cursor: pointer;',
-		//style: '',
+		height: 64,
+		width: 64,
+		//html: '<img id="LOGO" src="SRC/logo.png" width="64" height="64" style="opacity:0.75;"/>',
+		bodyStyle: 'background: none;',
+		style: 'position: absolute; z-index: 1100; background: none; right: 5px; bottom: 5px; width: 64px; height: 64px; background-image: url("SRC/logo.png");cursor: pointer; opacity: 0.9;',
 		hideBorders: true,
 		border: false
 };
